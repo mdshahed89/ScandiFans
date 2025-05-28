d0f6cc29b661fbccc60c076fd083a8d4ddd66335
mdshahed89 <mdshahedprofile@gmail.com>
2025-05-29 01:03:28 +0600

    trying 3

--- backend/utils/dbConnect.js
+++ backend/utils/dbConnect.js
@@ -1,19 +1,45 @@
+// import mongoose from "mongoose";
+
+// const dbConnect = async () => {
+//   try {
+//     await mongoose
+//       .connect(process.env.MONGODB_URI, {
+//         dbName: "ScandiFans",
+//       })
+//       .then(() => {
+//         console.log("Mongodb connected successfully");
+//       })
+//       .catch((err) => {
+//         console.log(`Mongodb connection error: ${err}`);
+//       });
+//   } catch (error) {
+//     console.log(`Failed to connect db`);
+//   }
+// };
+
+// export default dbConnect;
+
+
 import mongoose from "mongoose";
 
+let isConnected = false;
+
 const dbConnect = async () => {
+  if (isConnected) return;
+
   try {
-    await mongoose
-      .connect(process.env.MONGODB_URI, {
-        dbName: "ScandiFans",
-      })
-      .then(() => {
-        console.log("Mongodb connected successfully");
-      })
-      .catch((err) => {
-        console.log(`Mongodb connection error: ${err}`);
-      });
-  } catch (error) {
-    console.log(`Failed to connect db`);
+    const db = await mongoose.connect("mongodb+srv://RealEstate:RealEstate@realestate.n8dpbzv.mongodb.net/ScandiFans?retryWrites=true&w=majority&appName=RealEstate", {
+      dbName: "ScandiFans",
+      // useNewUrlParser: true,
+      // useUnifiedTopology: true,
+      serverSelectionTimeoutMS: 10000, 
+    });
+
+    isConnected = true;
+    console.log("MongoDB connected successfully");
+  } catch (err) {
+    console.error("MongoDB connection error:", err.message);
+    throw new Error("Failed to connect to database");
   }
 };
 
